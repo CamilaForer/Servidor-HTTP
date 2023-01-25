d9e851eb64e4bad6334d4bf4f9b767b836e0303f
Camila Forero <amandaforero@outlook.com>
2023-01-25 12:37:57 -0500

    Vercel config

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get("/",(req,res)=>{
-  res.send("Hola mundo")
-})
+  res.send("Hola mundo");
+});
 
 app.listen(port,()=>{
-  console.log("Mi puerto es "+ port)
-})
+  console.log("Mi puerto es "+ port);
+});
